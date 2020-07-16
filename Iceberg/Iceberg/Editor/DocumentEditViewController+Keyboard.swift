12498a565b9cb778b9225797da205387fb18c0d6
Luo Xu <ianluo63@gmail.com>
2020-07-16 22:52:27 +0800

    add file for handling keyboard events

---
+++ Iceberg/Iceberg/Editor/DocumentEditViewController+Keyboard.swift
@@ -0,0 +1,16 @@
+//
+//  DocumentEditViewController+Keyboard.swift
+//  x3Note
+//
+//  Created by ian luo on 2020/7/16.
+//  Copyright © 2020 wod. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension DocumentEditorViewController {
+    public override func pressesBegan(_ presses: Set<UIPress>, with event: UIPressesEvent?) {
+        
+    }
+}
