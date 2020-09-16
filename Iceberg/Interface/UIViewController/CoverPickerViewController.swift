b8e859c7c00ed615288526b7502e8e4b907665df
Luo Xu <ianluo63@gmail.com>
2020-09-16 22:54:52 +0800

    update pick image view for cover

--- Iceberg/Interface/UIViewController/CoverPickerViewController.swift
+++ Iceberg/Interface/UIViewController/CoverPickerViewController.swift
@@ -9,29 +9,90 @@
 import Foundation
 import UIKit
 
-public class CoverPickerViewController: UIImagePickerController {
+public class CoverPickerViewController: ActionsViewController {
     public var onSelecedCover: ((UIImage) -> Void)?
     public var onCancel: (() -> Void)?
     
     public override func viewDidLoad() {
+        self.showImageSourcePicker()
         super.viewDidLoad()
-        self.sourceType = .photoLibrary
-        self.delegate = self
+    }
+    
+    public func showCamera() {
+        let imagePicker = UIImagePickerController()
+        imagePicker.sourceType = .camera
+        imagePicker.delegate = self
+        self.present(imagePicker, animated: true, completion: nil)
+    }
+    
+    public func showImageLibrary() {
+        let imagePicker = UIImagePickerController()
+        imagePicker.sourceType = .photoLibrary
+        imagePicker.delegate = self
+        self.present(imagePicker, animated: true, completion: nil)
+    }
+    
+    // select from file system(mac) or files(iOS)
+    public func showDocumentPicker() {
+        let controller = UIDocumentPickerViewController(documentTypes: ["public.image"], in: UIDocumentPickerMode.import)
+        controller.delegate = self
+        
+        self.present(controller, animated: true, completion: nil)
+    }
+    
+    public func showImageSourcePicker() {
+        self.title = L10n.ImagePicker.add
+        
+        if UIImagePickerController.isSourceTypeAvailable(UIImagePickerController.SourceType.camera) {
+            self.addAction(icon: Asset.Assets.camera.image.fill(color: InterfaceTheme.Color.descriptive), title: L10n.ImagePicker.camera, action: { vc in
+                self.showCamera()
+            })
+        }
+        
+        self.addAction(icon: Asset.Assets.imageLibrary.image.fill(color: InterfaceTheme.Color.descriptive), title: L10n.ImagePicker.library, action: { vc in
+            self.showImageLibrary()
+        })
+        
+        self.addAction(icon: Asset.Assets.document.image.fill(color: InterfaceTheme.Color.descriptive), title: L10n.ImagePicker.files, action: { vc in
+            self.showDocumentPicker()
+        })
+    }
+}
+
+extension CoverPickerViewController: UIDocumentPickerDelegate {
+    public func documentPicker(_ controller: UIDocumentPickerViewController, didPickDocumentsAt urls: [URL]) {
+        controller.dismiss(animated: true) {
+            for url in urls {
+                if let image = UIImage(contentsOfFile: url.path) {
+                    let resizedImage = image.resize(upto: CGSize(width: 1024, height: 1024))
+                    self.dismiss(animated: true) { [unowned self] in
+                        self.onSelecedCover?(resizedImage)
+                    }
+                }
+            }
+        }
+    }
+    
+    public func documentPickerWasCancelled(_ controller: UIDocumentPickerViewController) {
+        controller.dismiss(animated: true)
     }
 }
 
 extension CoverPickerViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     public func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
-        self.dismiss(animated: true)
-        self.onCancel?()
+        picker.dismiss(animated: true)
     }
     
     public func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
-        if let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
-            let resizedImage = image.resize(upto: CGSize(width: 1024, height: 1024))
-            self.onSelecedCover?(resizedImage)
+        picker.dismiss(animated: true) {
+            if let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+                let resizedImage = image.resize(upto: CGSize(width: 1024, height: 1024))
+                self.dismiss(animated: true) { [unowned self] in
+                    self.onSelecedCover?(resizedImage)
+                }
+            }
+            
         }
-        
-        self.dismiss(animated: true, completion: nil)
+       
     }
 }
